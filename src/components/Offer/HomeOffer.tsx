b6483890ef360bdf680467ee0bbabd4bdd3d5738
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-04-25 00:20:12 +0200

    added avatar to home offer paper component

--- src/components/Offer/HomeOffer.tsx
+++ src/components/Offer/HomeOffer.tsx
@@ -1,48 +1,92 @@
 import React, { useState } from 'react'
-import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+//assets
+import BeautyTreatmentIcon from "../../assets/beauty-treatment.png"
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
 
+        root: {
+            display: 'flex',
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap: 'wrap',
+            height: "90vh",
+            '& > *': {
+                margin: theme.spacing(5),
+                width: theme.spacing(28),
+                height: theme.spacing(28),
+            },
+            [theme.breakpoints.down('sm')]: {
+                '& > *': {
+                    margin: theme.spacing(4),
+                    width: theme.spacing(24),
+                    height: theme.spacing(24),
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: 'wrap',
-        height: "90vh",
-        '& > *': {
-            margin: theme.spacing(5),
-            width: theme.spacing(28),
-            height: theme.spacing(28),
+                },
+            },
+            [theme.breakpoints.down('xs')]: {
+                height: "100%",
+            },
         },
-    },
-    paperCard: {
-        "&:hover": {
-            backgroundColor: "purple"
-        },
+        paperCard: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            transition: "all 3s easy-in-out",
+            border: "2px solid #f0f7f2",
 
-    }
-}));
+        },
+        title: {
+            fontSize: "21px"
+        },
+        avatar: {
+            height: "10rem",
+            width: "10rem"
+        }
+    }));
 
 const HomeOffer: React.FC = () => {
     const classes = useStyles();
-    const [shadow, setShadow] = useState<any>("outlined")
+    const [shadowFirst, setShadowFirst] = useState<any>("outlined")
+    const [shadowSecond, setShadowSecond] = useState<any>("outlined")
+    const [shadowThird, setShadowThird] = useState<any>("outlined")
+
+    const shadowFunc = (variantName: string, shadowName: string) => {
+        if (shadowName === "first") {
+            setShadowFirst(variantName)
+        }
+        if (shadowName === "second") {
+            setShadowSecond(variantName)
+        }
+        if (shadowName === "third") {
+            setShadowThird(variantName)
+        }
+    }
 
     return (
-        <Grid container className={classes.root} spacing={2}>
-            <Grid item sm={12} md={6} lg={3} >
-                <Paper elevation={3} className={classes.paperCard} />
-            </Grid>
-            <Grid item sm={12} md={6} lg={3} >
-                <Paper elevation={3} className={classes.paperCard} />
-            </Grid>
-            <Grid item sm={12} md={6} lg={3} >
-                <Paper elevation={3} className={classes.paperCard} />
-            </Grid>
+        <div className={classes.root}>
 
-        </Grid>
+            <Paper variant={shadowFirst} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "first")} onMouseLeave={() => shadowFunc("outlined", "first")} >
+                <Avatar alt="image" src={BeautyTreatmentIcon} className={classes.avatar} />
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Word of the Day
+        </Typography>
+
+            </Paper>
+
+
+            <Paper variant={shadowSecond} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "second")} onMouseLeave={() => shadowFunc("outlined", "second")} />
+
+
+            <Paper variant={shadowThird} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "third")} onMouseLeave={() => shadowFunc("outlined", "third")} />
+
+
+        </div>
     );
 }
 
