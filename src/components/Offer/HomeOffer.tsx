7ff197cc69f99011844494e8ebeb10d56dc19290
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-04-25 10:04:21 +0200

    added icons

--- src/components/Offer/HomeOffer.tsx
+++ src/components/Offer/HomeOffer.tsx
@@ -4,7 +4,9 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 //assets
-import BeautyTreatmentIcon from "../../assets/beauty-treatment.png"
+import Hand from "../../assets/hand.png";
+import Legs from "../../assets/legs.png";
+import Face from "../../assets/face.png";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -23,8 +25,8 @@ const useStyles = makeStyles((theme: Theme) =>
             [theme.breakpoints.down('sm')]: {
                 '& > *': {
                     margin: theme.spacing(4),
-                    width: theme.spacing(24),
-                    height: theme.spacing(24),
+                    width: theme.spacing(26),
+                    height: theme.spacing(26),
 
                 },
             },
@@ -35,19 +37,24 @@ const useStyles = makeStyles((theme: Theme) =>
         paperCard: {
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
+            justifyContent: "space-between",
             alignItems: "center",
             transition: "all 3s easy-in-out",
             border: "2px solid #f0f7f2",
 
         },
-        title: {
-            fontSize: "21px"
-        },
         avatar: {
             height: "10rem",
-            width: "10rem"
-        }
+            width: "10rem",
+            margin: "1.2rem 0 1.5rem 0",
+        },
+        title: {
+            textAlign: "center",
+            fontSize: "21px",
+            minHeight: "5rem",
+            margin: "auto 0",
+        },
+
     }));
 
 const HomeOffer: React.FC = () => {
@@ -72,18 +79,28 @@ const HomeOffer: React.FC = () => {
         <div className={classes.root}>
 
             <Paper variant={shadowFirst} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "first")} onMouseLeave={() => shadowFunc("outlined", "first")} >
-                <Avatar alt="image" src={BeautyTreatmentIcon} className={classes.avatar} />
+                <Avatar variant="square" alt="image" src={Face} className={classes.avatar} />
                 <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Word of the Day
+                    Zabiegi na twarz / Makijaż
         </Typography>
 
             </Paper>
 
 
-            <Paper variant={shadowSecond} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "second")} onMouseLeave={() => shadowFunc("outlined", "second")} />
+            <Paper variant={shadowSecond} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "second")} onMouseLeave={() => shadowFunc("outlined", "second")}>
+                <Avatar variant="square" alt="image" src={Legs} className={classes.avatar} />
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Depilacja
+        </Typography>
+            </Paper>
 
 
-            <Paper variant={shadowThird} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "third")} onMouseLeave={() => shadowFunc("outlined", "third")} />
+            <Paper variant={shadowThird} elevation={6} className={classes.paperCard} onMouseEnter={() => shadowFunc("elevation", "third")} onMouseLeave={() => shadowFunc("outlined", "third")}>
+                <Avatar variant="square" alt="image" src={Hand} className={classes.avatar} />
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Mezoterapia
+        </Typography>
+            </Paper>
 
 
         </div>
